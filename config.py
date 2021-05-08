061072768cfb0a3ad708f13e82ea2d619ec2a44c
Jared Dunn <jdunn@webucator.com>
2021-05-08 17:41:49 -0400

    Add create_api function

--- config.py
+++ config.py
@@ -1,16 +1,28 @@
 import tweepy
 import os
+import logging
 
 from dotenv import load_dotenv
 
-load_dotenv()
+logger = logging.getLogger()
 
-API_KEY = os.getenv("API_KEY")
-API_SECRET_KEY = os.getenv("API_SECRET_KEY")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+def create_api():
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    API_SECRET_KEY = os.getenv("API_SECRET_KEY")
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API")
+        raise e
+
+    return api
