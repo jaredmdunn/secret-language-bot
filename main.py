065271b6f022b93b1489dbc09b487303b4e9360c
Jared Dunn <jdunn@webucator.com>
2021-05-08 17:51:51 -0400

    Add main function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import tweepy
+import logging
 
+from config import create_api
+
+logger = logging.getLogger()
+
+
+def main():
+    api = create_api()
+    print("run main")
+
+
+if __name__ == "__main__":
+    main()
