9d47aef76b091763d4514ad3e774d97ab7108f0b
Jared Dunn <jdunn@webucator.com>
2021-05-09 23:18:24 -0400

    Respond to tweets in pig latin

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import tweepy
 import time
 
 from config import create_api
+from secret_languages import translate_text, LANGUAGES
 
 
 def check_mentions(api, since_id):
     print("check mentions")
-    new_since_id = since_id
+    user = api.me()
 
-    mentions = api.mentions_timeline(since_id=since_id)
+    new_since_id = since_id
+    mentions = api.mentions_timeline(since_id=since_id, tweet_mode="extended")
 
     for tweet in mentions:
         new_since_id = max(tweet.id, new_since_id)
-        print(f"{tweet.user.name} says {tweet.text}")
+        print(f"{tweet.user.name} says {tweet.full_text}")
 
-        if not tweet.user.following:
-            tweet.user.follow()
+        # if not tweet.user.following and not tweet.user.id == user.id:
+        #     tweet.user.follow()
+
+        language = "piglatin"
+        for hashtag in tweet.entities["hashtags"]:
+            if hashtag in LANGUAGES:
+                language = hashtag
+                break
+
+        response = translate_text(tweet.full_text, language=language)
+
+        if f"#{language}" not in response:
+            response += f" #{language}"
+
+        response = response.replace(
+            f"@{user.screen_name}", f"@{tweet.user.screen_name}"
+        )
 
         api.update_status(
-            status=f"Thanks for reaching out. We are not functional yet.",
+            status=response,
             in_reply_to_status_id=tweet.id,
             auto_populate_reply_metadata=True,
         )
