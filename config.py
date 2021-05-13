2dbec94ed12f510d57e0b83c3f7fb34ecc336b81
Jared Dunn <jdunn@webucator.com>
2021-05-13 11:43:12 -0400

    Test getting api_key

--- config.py
+++ config.py
@@ -6,11 +6,14 @@ from dotenv import load_dotenv
 
 def create_api():
     print("creating api")
+    print("loading dotenv")
     load_dotenv()
+    print("after load")
     API_KEY = os.getenv("API_KEY")
     API_SECRET_KEY = os.getenv("API_SECRET_KEY")
     ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
     ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+    print("api key: ", API_KEY)
 
     auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
